19bc740c568eb3f1c80774aba202f4bcf6b9cd5b
kodandareddy <kodandareddy21@gmail.com>
2022-02-19 19:50:19 +0530

    key removed

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react';
 import useStyles from './styles.js';
-
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import GreatPlace from '../MygreatPlace/greatplace'
 // AIza
-const Map = ({setCoords, setBounds, coords}) => {
-  console.log(coords)
+const Map = ({setCoords, setBounds, coords,places}) => {
   const classes = useStyles();
-
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
+  const isMobile = useMediaQuery('(min-width:600px)');
+   places=places.filter(place=> (!isNaN(place.latitude) && !isNaN(place.longitude)) )
+  console.log(places.forEach(pl=>{
+    console.log(pl.latitude,pl.longitude)
+  }),"}]]]]]]]]")
+   const defaultProps = {
+   
     zoom: 11
-  };  return (
+  };  
+  return (
+    <>
     <div className={classes.mapContainer}>
        <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAtvK_U6gQj3INAImZINlI8xA9gRdNrILw" }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
         defaultCenter={coords}
         defaultZoom={defaultProps.zoom}
         onChange={(e) => {
@@ -24,11 +28,60 @@ const Map = ({setCoords, setBounds, coords}) => {
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
       >
-      
+       { 
+         places?.map((place,i)=>(
+           <div 
+           className={classes.markerContainer} 
+           lat={Number(place.latitude )}
+           lng={Number(place.longitude)}
+           key={i}
+           >
+
+           {!isMobile
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                </Paper>
+              )}   
+           </div>
+         ))
+
+      } 
+        
       </GoogleMapReact>
+
     </div>
-    
+     {/* <GreatPlace 
+     text={'A'}
+      lat={59.955413} 
+      lng={30.337844}
+     /> */}
+     </>
+     
   )
 }
 
 export default Map
+
+
+// { !isMobile ? 
+//   <LocationOnOutlinedIcon color="primary" fontSize="large" />
+// : (<Paper elevation={3} >
+//        <Typography 
+//          className={classes.typography} 
+//          variant="subtitle2" 
+//          gutterBottom
+//        > 
+//        {place.name}
+//        </Typography>
+//        <img 
+//        src={place.photo 
+//        ? place.photo.images.large.url 
+//        : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'} className={classes.pointer}/>
+//    </Paper>)
+//    }
